106555082c5d89ed502bcbbb037fd27b8786289c
Dmitriy <skachok.d@gmail.com>
2022-07-21 20:15:27 +0300

    removind employees from the list

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -5,24 +7,42 @@ import WorkersList from '../workers-list/workers-list';
 import AppForm from '../app-form/app-form';
 import './app.css'
 
-function App() {
-    const employees = [
-        {name: "John D.", salary: 1500, increase: false, id: 1},
-        {name: "Mark R.", salary: 900, increase: true, id: 2},
-        {name: "Adam B.", salary: 1800, increase: false, id: 3},
-    ];
+class App extends Component{
+    constructor(props) {
+        super(props);
 
-    return (
-        <div className="app">
-            <AppInfo number={employees.length}/>
-            <div className="search-panel">
-                <SearchPanel />
-                <AppFilter />
+        this.state = {
+            employees: [
+                {name: "John D.", salary: 1500, increase: false, id: 1},
+                {name: "Mark R.", salary: 900, increase: true, id: 2},
+                {name: "Adam B.", salary: 1800, increase: false, id: 3},
+            ],
+        }
+    }
+
+    deleteItem = id => {
+        this.setState(({employees}) => ({
+                employees: employees.filter(item => item.id !== id),
+        }))
+    }
+
+    render() {
+        const { employees } = this.state;
+
+        return (
+            <div className="app">
+                <AppInfo number={employees.length}/>
+                <div className="search-panel">
+                    <SearchPanel />
+                    <AppFilter />
+                </div>
+                <WorkersList 
+                    employees={employees}
+                    onDelete={this.deleteItem}/>
+                <AppForm />
             </div>
-            <WorkersList employees={employees}/>
-            <AppForm />
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
--- src/components/workers-list/workers-list.js
+++ src/components/workers-list/workers-list.js
@@ -1,12 +1,15 @@
 import WorkersListItem from '../workers-list-item/workers-list-item';
 import './workers-list.css';
 
-const WorkersList = ({employees}) => {
+const WorkersList = ({employees, onDelete}) => {
 
     const elements = employees.map(employee => {
         const {id, ...restProps} = employee;
         return (
-            <WorkersListItem key={id} {...restProps}/>
+            <WorkersListItem 
+                key={id} 
+                {...restProps}
+                onDelete={() => onDelete(id)}/>
         );
     });
 
